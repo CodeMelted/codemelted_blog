3a5070d5808b45002072656026d393ddce962578
Mark Shaffer <31354065+CodeMelted@users.noreply.github.com>
2023-02-18 20:24:01 -0500

    Backing up last article in January.

---
+++ articles/2023/01/Chasing the Mythical Unicorn_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"https:\/\/www.codemelted.blog\/2023\/01\/chasing-mythical-unicorn.html","comments":0}]});
+}
